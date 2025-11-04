34519015549190a637d0d0291766cb8549ead3af
xsu6to8 <kyunsu00628@hanyang.ac.kr>
2025-11-04 13:52:12 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 300 ms, Memory: 3972 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a[500005];
+int n;
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> n;
+  for(int i = 0; i < n; i++) cin >> a[i];
+  sort(a,a+n);
+  int m;
+  cin >> m;
+  while(m--){
+    int t;
+    cin >> t;
+    cout << upper_bound(a,a+n,t)-lower_bound(a,a+n,t) << '\n';
+  }
+}

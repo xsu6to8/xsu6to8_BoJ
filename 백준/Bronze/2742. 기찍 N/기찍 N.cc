8870f70d13238d490638f0729d3c6d931913cc7a
xsu6to8 <kyunsu00628@hanyang.ac.kr>
2025-11-04 13:23:07 +0900

    [Bronze V] Title: 기찍 N, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int N;
+    cin >> N;
+    for (int i = N; i > 0; i--) {
+        cout << i << '\n';
+    }
+    return 0;
+}

78684edf9b8d9b1f91a8f1e6096c5fe3dd22fe0f
xsu6to8 <kyunsu00628@hanyang.ac.kr>
2025-11-04 14:27:59 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 356 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int T,a, b;
+
+    scanf("%d", &T);
+
+    for(int i = 0; i < T; i++)
+    {
+        scanf("%d %d", &a, &b);
+        printf("%d\n", a+b);
+    }
+
+    return 0;
+}

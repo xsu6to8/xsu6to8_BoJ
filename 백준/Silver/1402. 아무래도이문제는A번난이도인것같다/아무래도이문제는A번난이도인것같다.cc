196991f88fcd71e9b14e435ff2f54b47508d38fa
xsu6to8 <kyunsu00628@hanyang.ac.kr>
2025-11-04 12:59:03 +0900

    [Silver V] Title: 아무래도이문제는A번난이도인것같다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1402. 아무래도이문제는A번난이도인것같다/아무래도이문제는A번난이도인것같다.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    int a, b;
+
+    cin >> t;
+    while (t--)
+    {
+        cin >> a >> b;
+        cout << "yes" << endl;
+    }
+
+    // 음수 가능이기에
+    return 0;
+}

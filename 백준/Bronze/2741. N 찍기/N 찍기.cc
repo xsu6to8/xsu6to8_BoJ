2291ae7cd959e0666f22ac04cf0202b3eec60eea
xsu6to8 <kyunsu00628@hanyang.ac.kr>
2025-11-04 13:22:57 +0900

    [Bronze V] Title: N 찍기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+unordered_map<string, string> pw_pairs;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+        cout << i << '\n';
+
+    return 0;
+}

152a66385396be7ee9bb209c57367403fe3b8813
xsu6to8 <kyunsu00628@hanyang.ac.kr>
2025-11-04 13:24:40 +0900

    [Bronze III] Title: 학점계산, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	unordered_map<string, string> gpa = { { "A+", "4.3" }, { "A0", "4.0" }, { "A-", "3.7" },
+	{ "B+", "3.3" }, { "B0", "3.0" }, { "B-", "2.7" }, 
+	{ "C+", "2.3" }, { "C0", "2.0" }, { "C-", "1.7" },
+	{ "D+", "1.3" }, { "D0", "1.0" }, { "D-", "0.7" },
+	{ "F", "0.0" }
+	};
+
+	string s;
+	cin >> s;
+	cout << gpa[s];
+
+	return 0;
+}

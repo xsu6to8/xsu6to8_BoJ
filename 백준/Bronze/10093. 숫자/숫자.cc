7c93b8cd1efc6e55bde9b9f538245c4b969b4ea1
xsu6to8 <kyunsu00628@hanyang.ac.kr>
2025-11-04 13:47:42 +0900

    [Bronze II] Title: 숫자, Time: 12 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/10093. 숫자/숫자.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	long long a, b;
+	cin >> a >> b;
+
+	if (a > b) 
+		swap(a, b);
+	if (a == b || b - a == 1) 
+		cout << 0;
+	else {
+		cout << b - a - 1 << "\n";
+		for (long long i = a + 1; i < b; i++)
+			cout << i << " ";
+	}
+
+	return 0;
+}

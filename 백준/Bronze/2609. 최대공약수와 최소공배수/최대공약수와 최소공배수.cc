b3c9b7ec897b9fd7da674a064870908bc2f4a405
xsu6to8 <kyunsu00628@hanyang.ac.kr>
2025-11-04 13:21:10 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int lcm(int a, int b) {
+    return (a / gcd(a, b)) * b;
+}
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    cout << gcd(a, b) << "\n"; 
+    cout << lcm(a, b) << "\n";
+
+    return 0;
+}

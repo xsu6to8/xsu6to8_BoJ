49f8fabda16f8971abd5aa4c8fce1311c4c3ec07
xsu6to8 <kyunsu00628@hanyang.ac.kr>
2025-11-04 14:29:46 +0900

    [Silver III] Title: N과 M (8), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int arr[9];
+int result[9];
+
+
+void func(int cnt) {
+	if (cnt == m) {
+		for (int i = 0; i < m; i++)
+			cout << result[i] << ' ';
+
+		cout << '\n';
+		return;
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		if (cnt == 0) {
+			result[cnt] = arr[i];
+			func(cnt + 1);
+		}
+		else if (result[cnt - 1] <= arr[i])
+		{
+			result[cnt] = arr[i];
+			func(cnt + 1);
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+
+	sort(arr, arr + n);
+
+	func(0);
+}

225630cf4d1620520070fda867a9f25d49d382b8
xsu6to8 <kyunsu00628@hanyang.ac.kr>
2025-11-04 13:14:50 +0900

    [Bronze III] Title: 지능형 기차, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2455. 지능형 기차/지능형 기차.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num1, num2, total = 0, max = 0;
+
+	for (int i = 0; i < 4; i++) {
+		cin >> num1 >> num2;
+		total += num2 - num1;
+		if (total > max) {
+			max = total;
+		}
+	}
+	cout << max;
+}

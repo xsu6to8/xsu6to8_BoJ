28078bda3e9fbc1a0a26e749280f899c06dc4daf
xsu6to8 <kyunsu00628@hanyang.ac.kr>
2025-11-04 14:00:05 +0900

    [Bronze IV] Title: !밀비 급일, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11365. ！밀비 급일/！밀비 급일.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	string s;
+	while (true) {
+		getline(cin, s);
+		if (s == "END")
+			break;
+
+		reverse(s.begin(), s.end());
+		cout << s << endl;
+	}
+
+	return 0;
+}

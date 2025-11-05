aeef60af8cf2f438756c460e9a8e832537fa0ddd
xsu6to8 <kyunsu00628@hanyang.ac.kr>
2025-11-05 09:24:44 +0900

    [Silver V] Title: 투자의 귀재 배주형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/19947. 투자의 귀재 배주형/투자의 귀재 배주형.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int h, y;
+	cin >> h >> y;
+
+	vector<int> dp(y + 1, 0);
+	dp[0] = h;
+
+	for (int i = 1; i <= y; ++i) {
+		dp[i] = max(dp[i], (int)(dp[i - 1] * 1.05));
+
+		if (i >= 3)
+			dp[i] = max(dp[i], (int)(dp[i - 3] * 1.2));
+
+		if (i >= 5)
+			dp[i] = max(dp[i], (int)(dp[i - 5] * 1.35));
+	}
+
+	cout << dp[y];
+
+	return 0;
+}
